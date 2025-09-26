3ed2f895096b2b1baf5d8dbb9335ecafa21276d1
mitp1220 <mit214p@gmail.com>
2025-09-26 13:31:20 +0530

    Add files via upload

---
+++ Day-2_wpf/Day-2_wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Day_2_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
